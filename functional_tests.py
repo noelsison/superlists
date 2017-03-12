9cafa2f7a02ece0ead9f7a757adecffc5a2291cf
noelsison2 <nysison@gmail.com>
2017-03-12 18:05:30 +0800

    FT now checks if we can input a to-do item  - (FT) Functional Test

--- functional_tests.py
+++ functional_tests.py
@@ -1,35 +1,76 @@
+"""Functional Tests for superlists project
+
+User Stories are placed within their respective test_* functions
+Steps of the user story are enclosed in '''
+
+"""
+import time
 import unittest
+
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
+        # Selenium browser object we'll use to access the resulting webpage
         self.browser = webdriver.Firefox()
 
     def tearDown(self):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
+        """ Test User Story #1: Persistent To-Do List """
 
-        # Chi has once again heard about a cool new online to-do app. He goes
-        # to check out its home page
+        '''
+        Chi has once again heard about a cool new online to-do app. He goes
+        to check out its home page
+        '''
         self.browser.get('http://localhost:8000')
 
-        # He notices the page title and header mention to-do lists
-        self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        '''
+        He notices the page title and header mention to-do lists
+        '''
+        string_title = 'To-Do'
+        self.assertIn(string_title, self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn(string_title, header_text)
 
-        # He gets invited to enter a to-do item striaght away
+        '''
+        He gets invited to enter a to-do item striaght away
+        '''
+        string_instructions = 'Enter a to-do item'
+        inputbox = self.browser.find_element_by_id('id-new-item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            string_instructions
+        )
 
-        # He types "Reach 2k MMR" into a text box. (Chi's hobby is streaning
-        # some dotes)
+        '''
+        He types "Reach 2k MMR" into a text box. (Chi's hobby is streaning
+        some dotes)
+        '''
+        string_list_input = 'Reach 2k MMR'
+        inputbox.send_keys(string_list_input)
 
         # When he hits enter, the page updates, and now the page lists:
         # "1. Reach 2k MMR" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        string_list_item = '1: ' + string_list_input
+        table = self.browser.find_element_by_id('id-list-table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == string_list_item for row in rows)
+        )
+        # any(condition for iterable) return true if condition is true for
+        # any item in the iterable
 
         # There is still a text box inviting him to add another item. He
         # enters: "Stream some dotes" (Chi is a pro)
+        self.fail('Finish the test!')
 
         # The page updates again, and now shows both items on his list
 
