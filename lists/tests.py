f8ffd868d55b41b8f26331da6069d847690e09ba
noelsison2 <nysison@gmail.com>
2017-04-12 20:01:06 +0800

    New url for making a new list

--- lists/tests.py
+++ lists/tests.py
@@ -17,23 +17,20 @@ class HomePageTest(TestCase):
         # Check if it's using the expected template
         self.assertTemplateUsed(response, HOME_TEMPLATE_PATH)
 
-    def test_only_saves_item_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
 
+class NewListTest(TestCase):
     def test_can_save_a_POST_request(self):
-        self.client.post('/', data={'item_text': 'A new list item'})
+        self.client.post('/lists/new', data={'item_text': 'A new list item'})
 
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
     def test_redirects_after_POST_request(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'],
-                         '/lists/best-list-the-world-has-ever-seen/')
+        response = self.client.post(
+            '/lists/new', data={'item_text': 'A new list item'})
+        self.assertRedirects(
+            response, '/lists/best-list-the-world-has-ever-seen/')
 
 
 class ItemModelTest(TestCase):
--- lists/views.py
+++ lists/views.py
@@ -7,12 +7,14 @@ LIST_TEMPLATE_PATH = 'list.html'
 
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/best-list-the-world-has-ever-seen/')
     return render(request, HOME_TEMPLATE_PATH)
 
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, LIST_TEMPLATE_PATH, {'items': items})
+
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('view_list')
